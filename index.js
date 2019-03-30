f82ba42a8db858a56dd5dbd663c6adadd18cc87f
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-30 14:29:47 -0400

    adds ability to make get request to server by OrderId

--- index.js
+++ index.js
@@ -6,7 +6,39 @@ const Orders = require('./models/coffee-orders');
 const server = http.createServer(async(req, res) => {
     res.statusCode = 200; 
     res.setHeader('Content-Type', 'application/json');
+    if (req.url.startsWith("/orders")) {
+        const method = req.method;
+        const parts = req.url.split("/");
+        if (method === "GET"){
+            if (parts.length === 2) {
+                res.end('BeepBoop')
+            } else if (parts.length ===3) {
+                const orderId = parts[2];
+                const theOrder = await Orders.getById(orderId)
+                const orderJSON = JSON.stringify(theOrder);
+                res.end(orderJSON);
+            } else {
+                res.statusCode = 404;
+                res.end("Rekt");
+            }
+        }
 
+        else if (method === "POST"){
+            res.end(` {message: "Give me your bitcoins" }`);
+        }
+        else if (method === "PUT") {
+            res.end(`  {message: "What are you putting in here? Bitcoin?"}`);
+        }
+        else if (method === "DELETE") {
+            res.end(` {message: "delete dis"}`);
+        }
+        else {
+            res.end (` { 
+                message: "Why yes, I'll take ze bitoin."
+            }`)
+        }
+    
+    }
 
 
 })
