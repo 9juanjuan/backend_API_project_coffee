44c18f78951c193ae9b01cb7c7eecca171f3eddf
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-03-30 20:18:32 -0400

    updates users ORM to retrieve customer orders from coffee-orders table by user_id

--- models/coffee-orders.js
+++ models/coffee-orders.js
@@ -4,16 +4,16 @@ const User = require('./users')
 
 // Class constructor for each individuak order;
 class Order {
-    constructor (id, order, size, user_id ) {
+    constructor (id, order_name, size, user_id ) {
 
         this.id = id;
-        this.order = order;
+        this.order = order_name;
         this.size = size; 
         this.UserId= user_id;
     }
     // Get orders by ID
     static getById(id) {
-       return db.one(` select * from orders where id = ${id} `)
+       return db.one(` select * from orders where user_id = ${id} `)
         .then ((orderData) => {
             const newOrder = new Order(
                 orderData.id,
@@ -24,14 +24,31 @@ class Order {
             console.log(newOrder);
             return newOrder;
         })
+        .catch(() => {
+            return null; // signal an invalid value 
+        })
     }
-    //  Get all orders 
+    //  Returns array with all orders 
     static getAll() {
-
+        return db.any( `select * from orders `)
+            .then((OrdersArray) => {
+                return OrdersArray.map((ordersData)=> {
+                    const newOrders = new Order(
+                        ordersData.id,
+                        ordersData.order_name,
+                        ordersData.size,
+                        ordersData.user_id
+                    )
+                    console.log(newOrders)
+                    return newOrders
+                })
+              
+            })
     }
 
     // getById(1);
 
 }
 Order.getById(1);
+// console.log(Order.getAll())
 module.exports = Order; 
--- models/users.js
+++ models/users.js
@@ -12,16 +12,55 @@ class User {
         this.password = password; 
      }
      static getById(id) {
-
+        return db.one( `select * from users where id = ${id}`)
+            .then((userData) => {
+                const newUser = new User(
+                    userData.id,
+                    userData.first_name,
+                    userData.last_name,
+                    userData.email,
+                    userData.password
+                )
+                console.log(newUser)
+                return newUser;
+            })
+            .catch(() => {
+                return null; // signal an invalid value 
+            })
      }
      static getAll () {
 
      }
 // Retrieve customer orders by ID
-     get orders() {
+      orders() {
+         return db.any (` select * from orders where user_id = ${this.id}`)
+            .then((ordersArray) => {
+                 return ordersArray.map((orderData) => {
+                    const cust_orders = new Order(
+                        orderData.id,
+                        orderData.order_name,
+                        orderData.size,
+                        orderData.user_id
+                    )
+                    console.log(cust_orders)
+                    return cust_orders
+                })
+               
+            })
 
      }
     
 }
+// User.getById(1); 
+// Able to retrieve orders placed by specific customer
+
+
+async function customer_orders(id) {
+    const user1 = await User.getById(id);
+    const user1Order = await user1.orders();
+    // console.log(user1Order) 
+    return user1Order
+}
+customer_orders(2);
 
 module.exports = User; 
